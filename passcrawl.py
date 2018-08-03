41390ef8f9d05f45e6a79e4037a9996bc6fd791c
zadewg <38017709+zadewg@users.noreply.github.com>
2018-08-03 22:14:54 +0200

    Dictionary generation from Google crawling.

--- passcrawl.py
+++ passcrawl.py
@@ -32,7 +32,6 @@ def _getKeywords(url):
  
 USER_AGENT = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
  
- 
 def fetch_results(search_term, number_results, language_code):
     assert isinstance(search_term, str), 'Search term must be a string'
     assert isinstance(number_results, int), 'Number of results must be an integer'
