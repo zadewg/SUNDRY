7ad133ccacb1a481608462e6551af0d8bbc908d3
zadewg <38017709+zadewg@users.noreply.github.com>
2018-08-03 22:23:19 +0200

    Petersen-Lincoln index reliability analysis.

--- lincoln.py
+++ lincoln.py
@@ -1,48 +1,99 @@
-import random
+import matplotlib.pyplot as plt
+import random, time, sys
+from datetime import timedelta
 
-def do():
-	population = {}
+def main(pop, rate):
+    global start_time
+    global estimates
+    global number
 
-	number = 5000
-	number += 1
+    size = pop
+    iterations = 100
+    rate = rate
+        
+    percentage = rate
+    percentage_2 = rate
+    
+    start_time = time.time()
 
-	for _ in range(0,number):
-		population[_] = 0
+    estimates = []
 
-	marked = []
+    a = 0
+    while a < iterations:
+        try:
+            population = {}
+            number = size
+            number += 1
 
-	percentage = random.randint(17,23)
-	collect = int((number / 100) * percentage)
+            for i in range(0, number):
+                population[i] = 0
 
-	for _ in range(0, collect):
-		f = random.randint(0, number)
-		marked.append(f)
-		population[f] = 1
+            marked = []
+            
+            collect = int((number / 100) * percentage)
 
-	match = []
+            for i in range(0, collect):
+                f = random.randint(0, number)
+                marked.append(f)
+                population[f] = 1
 
-	percentage_2 = random.randint(17,23)
-	collect_2 = int((number / 100) * percentage_2)
+            match = []
+            
+            collect_2 = int((number / 100) * percentage_2)
 
-	for _ in range(0, collect_2):
-		f = random.randint(0, number)
-		#marked_2.append(f)
-		if population[f] == 1:
-			match.append(f)
+            for i in range(0, collect_2):
+                f = random.randint(0, number)
+                if population[f] == 1:
+                    match.append(f)
 
-		
-	print("marked: ", len(marked))
-	print("collected: ", collect_2)
-	print("match: ", len(match))
-	print("population: ", (number - 1))
-	print("lincoln index estimation: " + str((collect * collect_2)/(len(match))))
-	print ""
+            try:
+                lincoln = ((collect * collect_2) / (len(match)))
 
-i = 0
-while i < 10:
-	try:
-		do()
-		i += 1
+                estimates.append(lincoln)
 
-	except KeyError:
-		i = i
+            except ZeroDivisionError:
+                lincoln = “No matches”
+
+            print("\n[+] marked: ", len(marked))
+            print("[+] collected: ", collect_2)
+            print("[+] match: ", len(match))
+            print("[+] population: ", (number - 1))
+            print("[*] lincoln index estimates: ", lincoln)
+
+            a += 1
+            
+        except KeyError:
+            pass
+
+def processing(estimates, number, start_time):
+    total = 0
+    for i in estimates:
+        total += i
+
+    average = (total / len(estimates)
+
+    precision = ((average * 100) / (number - 1))
+
+    if precision < 100 and precision > 0:
+        precision = precision
+    else:
+        precision = (100 - (precision - 100))
+
+    print("\nPrecision: {}%".format(int(precision)))
+
+    elapsed_time = time.time() - start_time
+    times = str(timedelta(seconds=elapsed_time))
+    print("\nTime Elapsed:", str(times))
+
+    return precision
+
+results_effort = []
+results_pop = []
+
+def studies():
+    for i in range(1,101):
+        main(10000, i)
+        results_effort.append(processing(estimates, number, start_time))
+    for i in range(1,1001):
+        main(i, 10)
+        results_effort.append(processing(estimates, number, start_time))
